d4b082a0888b7a2616924f9f4ff76c718876c642
meghakaradbhajne <meghakaradbhajne>
2023-08-31 15:26:36 +0530

    first commit

--- dom.js
+++ dom.js
@@ -1,14 +1,7 @@
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+items[2].style.backgroundColor='green';
 
-//make ADD ITEM bold and chage the font color to greeen
-var titles = document.getElementsByClassName('title');
-console.log(titles);
-
-titles[0].style.fontWeight='bold';
-
-titles[0].style. color='green';
-
-//Make the title have a black border
-
-var headerTitle = document.getElementById('header-title');
-console.log(headerTitle);
-headerTitle.style.borderBottom = 'solid 3px #000';
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
