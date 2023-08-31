a1b86dbb3d61b51c28a0b93951b4e7c15b46f014
meghakaradbhajne <meghakaradbhajne>
2023-08-31 22:25:10 +0530

    first commit

--- dom.js
+++ dom.js
@@ -5,3 +5,10 @@ items[2].style.backgroundColor='green';
 for(var i=0;i<items.length;i++){
     items[i].style.fontWeight='bold';
 }
+
+//getElementByTagName
+
+var li=document.getElementsByTagName('li');
+for(var i=0;i<li.length;i++){
+    li[i].style.backgroundColor='#f4f4f4';
+}
