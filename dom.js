f29f89448248b3764f7f6f9421fb9513e8e19d36
meghakaradbhajne <meghakaradbhajne>
2023-08-31 14:56:22 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,14 @@
+
+//make ADD ITEM bold and chage the font color to greeen
+var titles = document.getElementsByClassName('title');
+console.log(titles);
+
+titles[0].style.fontWeight='bold';
+
+titles[0].style. color='green';
+
+//Make the title have a black border
+
+var headerTitle = document.getElementById('header-title');
+console.log(headerTitle);
+headerTitle.style.borderBottom = 'solid 3px #000';
